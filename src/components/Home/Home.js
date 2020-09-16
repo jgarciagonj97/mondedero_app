44d559b44c9bf10a1e514e618656ad7af854347f
jgarciagonj97 <j.garciagonj@gmail.com>
2020-09-16 19:44:07 +0200

    Retoques en Home. Falta controlar acceso.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
 const HomePage = () => {
 
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
     function handleSubmit(e) {
         e.preventDefault();
-    }
+        setEmail("");
+        setPassword("");
+    };
 
     return (
         <div className="card">
@@ -14,8 +19,10 @@ const HomePage = () => {
                     <h3>LOG-IN</h3>
                 </div>
                 <form className="formulario" onSubmit={handleSubmit}>
-                    <input type="text" placeholder="Escribe tu email" className="form-control email" />
-                    <input type="password" placeholder="Password" className="form-control pass" />
+                    <input type="text" placeholder="Escribe tu email" className="form-control email"
+                        value={email} onChange={(e) => { setEmail(e.target.value) }} />
+                    <input type="password" placeholder="Password" className="form-control pass"
+                        value={password} onChange={(e) => { setPassword(e.target.value) }} />
                     <button type="submit" className="btn btn-primary">Entrar</button>
                 </form>
             </div>
